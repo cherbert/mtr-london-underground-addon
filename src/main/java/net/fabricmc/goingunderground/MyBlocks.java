0dff490fc6bf03cf3ae030cf43cdb141ebed9659
cherbert <tinblue@msn.com>
2021-09-21 15:22:13 +0100

    Remove noCollision from Platform Block Improve Darkness Block

--- src/main/java/net/fabricmc/goingunderground/MyBlocks.java
+++ src/main/java/net/fabricmc/goingunderground/MyBlocks.java
@@ -11,7 +11,7 @@ import net.minecraft.block.Material;
 import net.minecraft.block.MapColor;
 
 public interface MyBlocks extends Blocks {
-    Block PLATFORM_TFL_1 = new BlockPlatform(FabricBlockSettings.of(Material.AIR, MapColor.YELLOW).requiresTool().hardness(2).air().noCollision(), true);
+    Block PLATFORM_TFL_1 = new BlockPlatform(FabricBlockSettings.of(Material.AIR, MapColor.YELLOW).requiresTool().hardness(2).air(), true);
     Block TUNNEL_DARKNESS = new TunnelDarknessBlock(FabricBlockSettings.of(Material.AIR, MapColor.OFF_WHITE).requiresTool().air().noCollision());
     Block TFL_BLOCK = new TFLBlock(FabricBlockSettings.of(Material.AIR, MapColor.OFF_WHITE).requiresTool().hardness(2));
 }
