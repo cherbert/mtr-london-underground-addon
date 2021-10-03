8adfd592ec5d8824f0cfd558726975622c77210a
cherbert <tinblue@msn.com>
2021-10-03 20:45:46 +0100

    Added /tfl command. Usage /tfl {CustomModelData}. Gives player custom model from IRON_INGOT

--- src/main/java/net/londonunderground/GiveCommand.java
+++ src/main/java/net/londonunderground/GiveCommand.java
@@ -1,38 +1,36 @@
 package net.londonunderground;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.command.argument.IdentifierArgumentType;
 import net.minecraft.entity.player.PlayerEntity;
-
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.tag.ItemTags;
-import net.minecraft.util.Identifier;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class GiveCommand {
-    static RequiredArgumentBuilder<ServerCommandSource, Identifier> requiredArgumentBuilder = CommandManager.argument("custom", IdentifierArgumentType.identifier());
+    static RequiredArgumentBuilder<ServerCommandSource, Integer> requiredArgumentBuilder = CommandManager.argument("custom", IntegerArgumentType.integer());
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(literal("tfl").then(requiredArgumentBuilder.executes(GiveCommand::run)));
         });
     }
 
-    public static int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+    public static int run(CommandContext<ServerCommandSource> context) {
         final PlayerEntity player = MinecraftClient.getInstance().player;
+        int modelData = IntegerArgumentType.getInteger(context,"custom");
 
-        System.out.println("Hello World!");
-        System.out.println(IdentifierArgumentType.getIdentifier(context,"custom"));
-        ItemStack chris = new ItemStack(Items.IRON_INGOT);
-        player.giveItemStack(chris);
+        ItemStack item = new ItemStack(Items.IRON_INGOT, 1);
+        NbtCompound compound = new NbtCompound();
+        compound.putInt("CustomModelData", modelData);
+        item.setTag(compound);
+        player.giveItemStack(item);
         return 1;
     }
 }
