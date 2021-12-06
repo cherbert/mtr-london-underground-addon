767d7aab17e9ebcf930a5c34ceb761369e3b8afd
Jonathan Ho <jonafanho@gmail.com>
2021-12-05 18:10:20 -0800

    Fixed tunnel entity renderer

--- src/main/java/net/londonunderground/entities/TunnelEntity.java
+++ src/main/java/net/londonunderground/entities/TunnelEntity.java
@@ -17,24 +17,16 @@ public class TunnelEntity extends EntityModel<Entity> {
 			final int textureHeight = 16;
 			final ModelDataWrapper modelDataWrapper = new ModelDataWrapper(this, textureWidth, textureHeight);
 			bb_main = new ModelMapper(modelDataWrapper);
-			bb_main.setPivot(0.0F, 24.0F, 0.0F);
-			bb_main.setTextureOffset(0, 0).addCuboid(0.0F, -24.0F, 0.0F, 16, 16, 16, 0.0F, false);
+			bb_main.setPivot(0, 24, 0);
+			bb_main.setTextureOffset(0, 0).addCuboid(0, -24, 0, 16, 16, 16, 0, false);
 			modelDataWrapper.setModelPart(textureWidth, textureHeight);
 			bb_main.setModelPart();
 	}
 	@Override
 	public void setAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
-			//previously the render function, render code was moved to a method below
 	}
 	@Override
 	public void render(MatrixStack matrixStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
-
 			bb_main.render(matrixStack, buffer, 0, 0, 0, packedLight, packedOverlay);
 	}
-	public void setRotationAngle(ModelPart bone, float x, float y, float z) {
-			bone.pitch = x;
-			bone.yaw = y;
-			bone.roll = z;
-	}
-
 }
