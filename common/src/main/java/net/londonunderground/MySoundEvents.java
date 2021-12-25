8d4e6f1c3fe7f555b7d15ca34c19ea286e4100ba
cherbert <tinblue@msn.com>
2021-12-25 01:52:25 +0000

    Fix Ambient Sounds not registering properly

--- common/src/main/java/net/londonunderground/MySoundEvents.java
+++ common/src/main/java/net/londonunderground/MySoundEvents.java
@@ -4,7 +4,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
 public interface MySoundEvents {
+
 	SoundEvent SOUND_EVENT_OUTSIDE_AMBIENT = new SoundEvent(new ResourceLocation(Main.MOD_ID, "cityambient"));
 	SoundEvent SOUND_EVENT_SEE_IT_SAY_IT = new SoundEvent(new ResourceLocation(Main.MOD_ID,"seeitsayitsorted"));
 	SoundEvent SOUNT_EVENT_TUBE_STATION_AMBIENCE1 = new SoundEvent(new ResourceLocation(Main.MOD_ID, "ambient1"));
+
 }
