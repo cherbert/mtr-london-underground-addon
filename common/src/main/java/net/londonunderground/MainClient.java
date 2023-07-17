8e34d097f2227a03931027082cb7a87250ade20f
cherbert <tinblue@gmail.com>
2023-07-17 16:59:51 +0100

    Add new Signal Block rendering to EntityTypes and Client

--- common/src/main/java/net/londonunderground/MainClient.java
+++ common/src/main/java/net/londonunderground/MainClient.java
@@ -12,6 +12,7 @@ public class MainClient {
 	public static void init() {
 		RegistryClient.registerTileEntityRenderer(MyBlockEntityTypes.DARK_TILE.get(), RenderDarkTile::new);
 		RegistryClient.registerTileEntityRenderer(MyBlockEntityTypes.TUNNEL_BLOCK_2_SIGNAL.get(), dispatcher -> new RenderTunnelSignalLight<>(dispatcher, true, false, 0xFF00FF00));
+		RegistryClient.registerTileEntityRenderer(MyBlockEntityTypes.TUNNEL_A2_SIGNAL.get(), dispatcher -> new RenderTunnelSignalLight<>(dispatcher, true, false, 0xFF00FF00));
 		RegistryClient.registerTileEntityRenderer(MyBlockEntityTypes.PIDS_NORTHERN_TILE_ENTITY.get(), dispatcher -> new RenderPIDS<>(dispatcher, 3, 1.5F, 7.5F, 6, 6.5F, 29, true, true, 0xFF9900, 0xFF9900));
 		RegistryClient.registerTileEntityRenderer(MyBlockEntityTypes.BLOCK_ROUNDEL_TILE_ENTITY.get(), dispatcher -> new RenderRoundel<>(dispatcher, 14 / 16F, 0.2F / 16, 0, 0, -0.5F, 0, 0xFFB3B3B3, false, "johnston"));
 		RegistryClient.registerTileEntityRenderer(MyBlockEntityTypes.BLOCK_ROUNDEL_NLE_TILE_ENTITY.get(), dispatcher -> new RenderRoundel<>(dispatcher, 14 / 16F, 0.2F / 16, 0, 0, -0.495F, 0, 0xFFB3B3B3, false, "johnston"));
@@ -50,6 +51,7 @@ public class MainClient {
 		RegistryClient.registerTileEntityRenderer(MyBlockEntityTypes.SIGN_PRIDE_TILE_ENTITY.get(), dispatcher -> new RenderRoundel<>(dispatcher, 15 / 16F, 0.2F / 16, 0, 10.2F / 16, 0.425F / 16, 0, 0xFFB3B3B3, true, "johnston"));
 
 		RegistryClient.registerBlockRenderType(RenderType.cutout(), MyBlocks.TUNNEL_BLOCK_2_SIGNAL.get());
+		RegistryClient.registerBlockRenderType(RenderType.cutout(), MyBlocks.TUNNEL_A2_SIGNAL.get());
 		RegistryClient.registerBlockRenderType(RenderType.cutout(), MyBlocks.BRITISH_RAIL_UNDERGROUND.get());
 
 		RegistryClient.registerBlockRenderType(RenderType.cutout(), MyBlocks.BLOCK_ROUNDEL_1.get());
--- common/src/main/java/net/londonunderground/blocks/TunnelA2Signal.java
+++ common/src/main/java/net/londonunderground/blocks/TunnelA2Signal.java
@@ -1,25 +1,34 @@
 package net.londonunderground.blocks;
 
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import mtr.block.BlockSignalLightBase;
+import mtr.block.IBlock;
+import net.londonunderground.MyBlockEntityTypes;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class TunnelA2Signal extends HorizontalDirectionalBlock {
-	public TunnelA2Signal(Properties settings) {
-		super(settings);
+public class TunnelA2Signal extends BlockSignalLightBase {
+	public TunnelA2Signal(BlockBehaviour.Properties settings) {
+		super(settings, 2, 14);
 	}
 
 	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-		return defaultBlockState().setValue(FACING, ctx.getHorizontalDirection());
+	public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext collisionContext) {
+		return IBlock.getVoxelShapeByDirection(0, 0, 0, 16, 16, 16, IBlock.getStatePropertySafe(state, FACING));
 	}
 
 	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(FACING);
+	public mtr.mappings.BlockEntityMapper createBlockEntity(BlockPos pos, BlockState state) {
+		return new TileEntityTunnelSignalLight1(pos, state);
 	}
 
+	public static class TileEntityTunnelSignalLight1 extends mtr.mappings.BlockEntityMapper {
 
+		public TileEntityTunnelSignalLight1(BlockPos pos, BlockState state) {
+			super(MyBlockEntityTypes.TUNNEL_A2_SIGNAL.get(), pos, state);
+		}
+	}
 }
