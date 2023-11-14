45089c50111303122710d5d1f5eb6792e3c12454
cherbert <tinblue@gmail.com>
2023-11-14 22:53:28 +0000

    Fix Station Light incorrect Orientation. Bump version for release.

--- common/src/main/java/net/londonunderground/blocks/StationLight.java
+++ common/src/main/java/net/londonunderground/blocks/StationLight.java
@@ -6,7 +6,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
@@ -14,8 +13,9 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class StationLight extends HorizontalDirectionalBlock {
+public class StationLight extends Block {
 
+	public static final BooleanProperty FACING = BooleanProperty.create("facing");
 
 	public StationLight(Properties settings) {
 		super(settings);
@@ -23,17 +23,26 @@ public class StationLight extends HorizontalDirectionalBlock {
 
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-		return defaultBlockState().setValue(FACING, ctx.getHorizontalDirection());
+		boolean facing = ctx.getHorizontalDirection().getAxis() == Direction.Axis.X;
+		return defaultBlockState().setValue(FACING, facing);
 	}
 
 	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext collisionContext) {
-		final Direction facing = IBlock.getStatePropertySafe(state, FACING).getClockWise();
-		return Shapes.or(
-				IBlock.getVoxelShapeByDirection(6,14.5,0,6.5, 16, 16, facing),
-				IBlock.getVoxelShapeByDirection(9.5, 14.5, 0,10, 16, 16, facing),
-				IBlock.getVoxelShapeByDirection(6.5, 15, 0,9.5, 16, 16, facing)
-		);
+	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext collisionContext) {
+		if (IBlock.getStatePropertySafe(state, FACING)) {
+			return Shapes.or(
+					Block.box(0, 14.5, 6, 16, 16, 6.5),
+					Block.box(0, 14.5, 9.5, 16, 16, 10),
+					Block.box(0, 15, 6.5, 16, 16, 9.5)
+			);
+		} else {
+
+			return Shapes.or(
+					Block.box(6, 14.5, 0, 6.5, 16, 16),
+					Block.box(9.5, 14.5, 0, 10, 16, 16),
+					Block.box(6.5, 15, 0, 9.5, 16, 16)
+			);
+		}
 	}
 
 	@Override
