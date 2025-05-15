bf6a86143f7d224da5bdeec3e32b69036be26c47
Jonathan Ho <jonafanho@gmail.com>
2025-05-15 00:43:51 -0700

    Show "Due" on PIDS when the arrival is less than one minute

--- fabric/src/main/java/net/londonunderground/mod/render/RenderNorthernLinePIDS.java
+++ fabric/src/main/java/net/londonunderground/mod/render/RenderNorthernLinePIDS.java
@@ -8,6 +8,7 @@ import org.mtr.mapping.holder.Style;
 import org.mtr.mapping.mapper.GraphicsHolder;
 import org.mtr.mapping.mapper.TextHelper;
 import org.mtr.mod.config.Config;
+import org.mtr.mod.generated.lang.TranslationProvider;
 import org.mtr.mod.render.RenderPIDS;
 
 public class RenderNorthernLinePIDS extends RenderPIDS<NorthernLinePIDS.TileEntityNorthernLinePIDS> {
@@ -21,5 +22,14 @@ public class RenderNorthernLinePIDS extends RenderPIDS<NorthernLinePIDS.TileEnti
 		final Style style = Config.getClient().getUseMTRFont() ? Style.getEmptyMapped().withFont(new Identifier(Init.MOD_ID, "london-underground-regular")) : Style.getEmptyMapped();
 		final OrderedText orderedText = TextHelper.mutableTextToOrderedText(TextHelper.setStyle(TextHelper.literal(text), style));
 		graphicsHolder.drawText(orderedText, x, y, color, false, GraphicsHolder.getDefaultLight());
+	}
+
+	@Override
+	public String getArrivalString(long arrival, boolean isRealtime, boolean isCjk) {
+		if (arrival < 60 && !isCjk) {
+			return TextHelper.translatable("gui.londonunderground.arrival_sec").getString();
+		} else {
+			return super.getArrivalString(arrival, isRealtime, isCjk);
+		}
 	}
 }
